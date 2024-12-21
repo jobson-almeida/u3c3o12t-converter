c76f5f4a264b3ada35b02bc1c0941c1174359650
Lucas Silva <125084280+zSukita@users.noreply.github.com>
2024-12-21 08:58:55 -0300

    Update main.c
    revisão código de conversão de potencia

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <locale.h>
 
 void unidade_simples(float value);
-void unidade_com_submenu();
 void unidade_com_submenu_comprimento();
+void unidade_com_submenu_temperatura();
 void unidade_com_submenu_potencia();
 void convert_power(float value, int from_unit, int to_unit);
+
 
 int main()
 {
@@ -45,19 +46,10 @@ int main()
             unidade_com_submenu_comprimento();
             break;
         case 2:
-            printf("digite um valor: ");
-            if (scanf("%f", &value) == 0)
-            {
-                float opt;
-                // remove a entrada inválida do buffer de entrada
-                while ((opt = getchar()) != '\n' && opt != EOF);
-                printf("valor inválido!\n");
-                break;
-            }
-            unidade_simples(value);
+            unidade_com_submenu_massa();
             break;
         case 4:
-            unidade_com_submenu();
+            unidade_com_submenu_temperatura();
             break;
         case 5:
             break;
@@ -65,19 +57,19 @@ int main()
             unidade_com_submenu_potencia();
             break;
         case 9:
-    printf("digite um valor: ");
-    float option;  // Declare option como float
-    if (scanf("%f", &option) == 0)
-    {
-        int opt;  // Esta variável pode continuar como int pois é só para limpar o buffer
-        // remove a entrada inválida do buffer de entrada
-        while ((opt = getchar()) != '\n' && opt != EOF);
-        printf("valor inválido!\n");
-        break;
-    }
-            printf("faça a conversão\n");
             break;
         default:
+            // ESTE BLOCO DE CÓDIGO SERVE DE MODELO
+            // printf("digite um valor: ");
+            // if (scanf("%f", &option) == 0)
+            // {
+            //     float opt;
+            //     // remove a entrada inválida do buffer de entrada
+            //     while ((opt = getchar()) != '\n' && opt != EOF);
+            //     printf("valor inválido!\n");
+            //     break;
+            // }
+            // printf("faça a conversão\n");
             printf("opção não encontrada!\n");
             break;
         }
@@ -86,62 +78,65 @@ int main()
     return 0;
 }
 
-void unidade_simples(float value)
+/*ESTE BLOCO DE CÓDIGO SERVE DE MODELO PARA TODAS AS CONVERSÕES
+
+void unidade_nomeDaSuaGrandeza(float value)
 {
-    printf("faça a conversão pela função\n");
+    printf("faça a conversão função\n");
 }
 
-void unidade_com_submenu()
-{
-    int option = -1;
-    float value = 0.0;
-
-    while (option != 0)
+    void unidade_com_submenu_nomeDaSuaGrandeza()
     {
-        printf("\n");
-        printf(":::: Comprimento :::::::::::::::::::::::::::::::::::\n");
-        printf("::                                                ::\n");
-        printf(":: 1. metro -> centímetro                         ::\n");
-        printf(":: 2. centímetro -> metro                         ::\n");
-        printf(":: 3. opção 3                                     ::\n");
-        printf(":: 0. sair                                        ::\n");
-        printf("::                                                ::\n");
-        printf("::::::::::::::::::::::::::::::::::::::::::::::::::::\n\n");
+        int option = -1;
+        float value = 0.0;
 
-        printf("digite uma opção: ");
-        if (scanf("%d", &option) == 0)
+        while (option != 0)
         {
-            int opt;
-            // remove a entrada inválida do buffer de entrada
-            while ((opt = getchar()) != '\n' && opt != EOF);
-            option = -1;
-        }
+            printf("\n");
+            printf(":::: Grandeza ::::::::::::::::::::::::::::::::::::::\n");
+            printf("::                                                ::\n");
+            printf(":: 1. Exemplo 1                                   ::\n");
+            printf(":: 2. Exemplo 2                                   ::\n");
+            printf(":: 3. Exemplo 3                                   ::\n");
+            printf(":: 0. sair                                        ::\n");
+            printf("::                                                ::\n");
+            printf("::::::::::::::::::::::::::::::::::::::::::::::::::::\n\n");
 
-        if (option != 0)
-        {
-            switch (option)
+            printf("digite uma opção: ");
+            if (scanf("%d", &option) == 0)
             {
-            case 1:
-            case 2:
-            case 3:
-                printf("digite um valor: ");
-                if (scanf("%f", &option) == 0)
+                int opt;
+                // remove a entrada inválida do buffer de entrada
+                while ((opt = getchar()) != '\n' && opt != EOF);
+                option = -1;
+            }
+
+            if (option != 0)
+            {
+                switch (option)
                 {
-                    float opt;
-                    // remove a entrada inválida do buffer de entrada do usuário
-                    while ((opt = getchar()) != '\n' && opt != EOF);
-                    printf("valor inválido!\n");
+                case 1:
+                case 2:
+                case 3:
+                    printf("digite um valor: ");
+                    if (scanf("%f", &option) == 0)
+                    {
+                        float opt;
+                        // remove a entrada inválida do buffer de entrada do usuário
+                        while ((opt = getchar()) != '\n' && opt != EOF);
+                        printf("valor inválido!\n");
+                        break;
+                    }
+                    printf("faça a conversão\n");
+                    break;
+                default:
+                    printf("opção não encontrada!\n");
                     break;
                 }
-                printf("faça a conversão\n");
-                break;
-            default:
-                printf("opção não encontrada!\n");
-                break;
             }
         }
     }
-}
+*/
 
 //  -----------------------------UNIDADES DE COMPRIMENTO----------------------------------
 
@@ -264,8 +259,107 @@ void unidade_com_submenu_comprimento()
             }
         }
     }
+} 
+
+//  -----------------------------CONVERSÃO DE TEMPERATURA----------------------------------
+
+//Este bloco realiza a conversão de temperatura para Celsius, Fahrenheit, Kelvin ou outras escalas desejadas
+
+//Declaração das funções que fazem a conversão entre as unidades de temperatura Celsius, Fahrenheit e Kelvin
+float celsius_para_fahrenheit(float celsius) {
+    return (celsius * 9/5) + 32;
 }
 
+float celsius_para_kelvin(float celsius) {
+    return (celsius + 273.15);
+}
+
+float fahrenheit_para_kelvin(float fahrenheit) {
+    return ((fahrenheit - 32) * 5/9 + 273.15);
+}
+
+float fahrenheit_para_celsius(float fahrenheit) {
+    return ((fahrenheit - 32) * 5/9);
+}
+
+float kelvin_para_celsius(float kelvin) {
+    return (kelvin - 273.15);
+}
+
+float kelvin_para_fahrenheit(float kelvin) {
+    return ((kelvin - 273.15) * 9/5 + 32);
+}
+
+//Inicialização da função que mostrará o menu e os resultados somente da unidade de temperatura
+void unidade_com_submenu_temperatura() {
+    
+    int option = -1;
+    float temperatura, resultado;
+
+    //Esse laço tem a funcionalidade de abrir um menu com as opções de entrada
+    while (option != 0) {
+        printf("\n::::::: Temperatura ::::::::::::::::::::::::::::::::\n"
+               "::                                                ::\n"
+               ":: 1. Celsius -> Fahrenheit                       ::\n"
+               ":: 2. Celsius -> Kelvin                           ::\n"
+               ":: 3. Fahrenheit -> Kelvin                        ::\n"
+               ":: 4. Fahrenheit -> Celsius                       ::\n"
+               ":: 5. Kelvin -> Celsius                           ::\n"
+               ":: 6. Kelvin -> Fahrenheit                        ::\n"
+               ":: 0. Sair                                        ::\n"
+               "::                                                ::\n"
+               ":::::::::::::::::::::::::::::::::::::::::::::::::::: \n\n");
+
+        printf("Digite uma opção: ");
+
+        if (scanf("%d", &option) == 0) {
+            int opt;
+            // Remover a entrada inválida do buffer de entrada
+            while ((opt = getchar()) != '\n' && opt != EOF);
+            option = -1;
+        } 
+        else {
+            if (option != 0) {
+                printf("\nDigite a temperatura a ser convertida: ");
+                if (scanf("%f", &temperatura) == 0) {
+                    int opt;
+                    while ((opt = getchar()) != '\n' && opt != EOF);
+                    continue;
+                }
+
+                switch (option) {
+                    case 1:
+                        resultado = celsius_para_fahrenheit(temperatura);
+                        printf("Resultado: %.2f °F\n", resultado);
+                        break;
+                    case 2:
+                        resultado = celsius_para_kelvin(temperatura);
+                        printf("Resultado: %.2f K\n", resultado);
+                        break;
+                    case 3:
+                        resultado = fahrenheit_para_kelvin(temperatura);
+                        printf("Resultado: %.2f K\n", resultado);
+                        break;
+                    case 4:
+                        resultado = fahrenheit_para_celsius(temperatura);
+                        printf("Resultado: %.2f °C\n", resultado);
+                        break;
+                    case 5:
+                        resultado = kelvin_para_celsius(temperatura);
+                        printf("Resultado: %.2f °C\n", resultado);
+                        break;
+                    case 6:
+                        resultado = kelvin_para_fahrenheit(temperatura);
+                        printf("Resultado: %.2f °F\n", resultado);
+                        break;
+                    default:
+                        printf("Opção não encontrada!\n");
+                        break;
+                }
+            }
+        }
+    }
+}
 //  -----------------------------UNIDADES DE POTENCIA----------------------------------
 
 // Conversão de potência
